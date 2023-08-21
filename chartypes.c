5b041c7453608cc37e7e191a5f80fdec83da21d5
Mr. Bread <salekbenelhabchi@gmail.com>
2023-08-21 19:46:48 +0100

    use one file for the2

---
+++ chartypes.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_str - print a string.
+ * @ar: char * argument.
+ *
+ * Return: The length of string.
+ */
+int print_str(va_list ar)
+{
+	char *tmp = va_arg(ar, char *);
+	int i;
+
+	if (tmp == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	for (i = 0; tmp[i] != '\0'; i++)
+		_putchar(tmp[i]);
+	return (i);
+}
+/**
+ * print_char - prints a char.
+ * @ar: a given char arg.
+ *
+ * Return: 1 wich is the number of char printed.
+ */
+int print_char(va_list ar)
+{
+	char c = va_arg(ar, int);
+
+	_putchar(c);
+	return (1);
+}
